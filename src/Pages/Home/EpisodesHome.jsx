d350c5503cd4879d1613267676b543f2beb1bb49
nahid7g <nahid.h.kiran@gmail.com>
2024-09-13 10:19:57 +0600

    Home page done

--- src/Pages/Home/EpisodesHome.jsx
+++ src/Pages/Home/EpisodesHome.jsx
@@ -6,12 +6,12 @@ const EpisodesHome = () => {
     <div className="home-episodes pb-24">
       <div className="container">
         <h2 className="section-title">Episodes</h2>
-        <div className="grid grid-cols-3 mt-3 items-center">
+        <div className="sm:grid sm:grid-cols-3 mt-3 items-center">
           <p className="col-span-2">
             Sed laoreet diam sagittis tempus convallis. Interdum et malesuada
             fames ac ante ipsum primis in faucibus.
           </p>
-          <div className="text-end">
+          <div className="flex justify-end">
             <Link to="/" className="primary-btn">
               View All
             </Link>
